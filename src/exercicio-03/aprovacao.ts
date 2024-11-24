221a54fe7923f0c3cb42d866d1b0a0009361ee1b
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-24 10:51:25 -0300

    Exercício 3

--- src/exercicio-03/aprovacao.ts
+++ src/exercicio-03/aprovacao.ts
@@ -1,7 +1,30 @@
+/*
+Problema
+Você está desenvolvendo um programa de avaliação de alunos. 
+O programa deve, a partir dos dados de um aluno consultado (matrícula, nome, média) pelo professor, dizer se ele foi aprovado ou reprovado. 
+Alunos aprovados são aqueles que possuem média de, no mínimo, 6.
+
+Entrada
+A entrada será uma variável será composta por uma variável do tipo objeto, chamada aluno, que possue três propriedades:
+
+matricula: do tipo string que contém o número da matricula do aluno
+nome: do tipo string que contém o nome do aluno
+media: do tipo number que contém a média do aluno
+Saída
+Seu programa deve RETORNAR:
+
+APROVADO: caso o aluno tenha sido aprovado
+REPROVADO: caso o aluno tenha sido reprovado
+*/
+
 type TAluno = { matricula: string; nome: string; media: number };
 
 function solucao(aluno: TAluno): string {
   // seu código aqui
+  if(aluno.media < 6){
+    return 'REPROVADO'
+  }
+  return 'APROVADO'
 }
 
 export default solucao;
